a25daecb24d5627dc3cc830c2d220d4f5f042168
phylix <alirezarastegar117390@gmail.com>
2023-02-22 22:51:50 +0330

    27 --> Creating dto class for create object

---
+++ final_project_phase3/src/main/java/com/example/dto/comment/CreateCommentDto.java
@@ -0,0 +1,31 @@
+package com.example.dto.comment;
+
+
+import com.example.entity.Orders;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CreateCommentDto {
+
+    @NotNull(message = "the orders must not be empty.")
+    Orders orders;
+
+    @NotBlank(message = "email cannot be empty.")
+    @Email(message = "the email pattern is incorrect.")
+    String email;
+
+    @Positive(message = "score cannot be negative or zero.")
+    @NotNull(message = "score cannot be empty.")
+    @Min(value = 1, message = "the score must be between 1 and 5.")
+    @Max(value = 5, message = "the score must be between 1 and 5.")
+    @Digits(integer = 1, fraction = 0, message = "the score must be a number.")
+    Integer score;
+
+    String content;
+}
---
+++ final_project_phase3/src/main/java/com/example/dto/payment/CreatePaymentDto.java
@@ -0,0 +1,21 @@
+package com.example.dto.payment;
+
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CreatePaymentDto {
+
+    @NotBlank(message = "email cannot be empty.")
+    @Email(message = "the email pattern is incorrect.")
+    String email;
+
+    @NotNull(message = "the order id must not be empty.")
+    Long id;
+}
