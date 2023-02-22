d4122628e0d7624a692612bac1cb974c28a81d9f
phylix <alirezarastegar117390@gmail.com>
2023-02-22 22:32:28 +0330

    20 --> Creating the CommentRepository interface to communicate with the under comment table

---
+++ final_project_phase3/src/main/java/com/example/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.example.repository;
+
+import com.example.entity.Comments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comments, Long> {
+
+    @Query("select c from Comments c where c.expert.id= ?1")
+    List<Comments> findExpertComment(Long id);
+}
