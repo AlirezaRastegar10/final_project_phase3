98a007cb6bf2a7e396acd9cd17d59565b0961052
phylix <alirezarastegar117390@gmail.com>
2023-02-22 22:28:05 +0330

    16 --> Creating the ExpertRepository interface to communicate with the expert table.

---
+++ final_project_phase3/src/main/java/com/example/repository/ExpertRepository.java
@@ -0,0 +1,25 @@
+package com.example.repository;
+
+import com.example.entity.Expert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface ExpertRepository extends JpaRepository<Expert, Long> {
+
+    Optional<Expert> findByEmail(String email);
+
+    @Query("select e from Expert e where e.status='NEW'")
+    List<Expert> findAllByStatus();
+
+    @Query("select e from Expert e where e.id= ?1 and e.status='NEW'")
+    Optional<Expert> findExpertById(Long id);
+
+    @Query("select e from Expert e where e.email= ?1 and e.status='ACCEPTED'")
+    Optional<Expert> findByEmailAndStatus(String email);
+}
